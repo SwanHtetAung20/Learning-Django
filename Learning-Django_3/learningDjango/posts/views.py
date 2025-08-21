da1f36ed78ac50beeeaf2fd42d965f4b6155884a
Swan Htet Aung <swanhtetaung308@gmail.com>
2025-08-21 23:45:03 +0700

    using urls and slugs

--- Learning-Django_3/learningDjango/posts/views.py
+++ Learning-Django_3/learningDjango/posts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post.html', {'post': post})
